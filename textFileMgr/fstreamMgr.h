4af4bfdb61de155b9fca98081e9579562daca6c6
qelover <weiqin_wq@163.com>
2013-07-24 23:09:40 +0800

    Create fstreamMgr.h
    维护一系列文件的打开句柄，节省不断打开关闭文件的开销

---
+++ textFileMgr/fstreamMgr.h
@@ -0,0 +1,48 @@
+#ifndef FSTREAM_MANAGER_H
+#define FSTREAM_MANAGER_H
+
+#include <fstream>
+
+#include "hash_map.h"
+
+using namespace std;
+
+
+namespace MapLocation
+{
+class FStreamMgr
+{
+  public:
+		static FStreamMgr* getInstance();
+		
+		static void Destroy();
+
+		fstream* getFStream(const string& fileName);
+
+		void addFStream(const string& fileName, fstream* pFStream);
+		
+		hash_map<string, fstream*>& getAllFStream() {return m_fsHashMap; }
+
+	private:
+		FStreamMgr();
+		
+		~FStreamMgr();
+		
+	private:
+		static hash_map<string, fstream*> m_fsHashMap;
+
+		static FStreamMgr* m_cpFStreamMgr;
+
+		class DestroyFStreamMgr
+		{
+			public:
+				~DestroyFStreamMgr()
+				{
+					FStreamMgr::Destroy();
+				}
+		};
+		static DestroyFStreamMgr cDesFStreamMgr;
+};
+};
+
+#endif
