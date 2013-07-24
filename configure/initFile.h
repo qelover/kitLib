7fb1c9921c40bab91589deff236caecbe97ffc2a
qelover <weiqin_wq@163.com>
2013-07-24 23:57:37 +0800

    Update initFile.h

--- configure/initFile.h
+++ configure/initFile.h
@@ -1,3 +1,9 @@
+/*
+  @brief: get key-value pairs from init file
+  @attention: open first, then use get()
+*/
+
+
 #ifndef INIFILE_H
 #define INIFILE_H
 
@@ -9,31 +15,31 @@
 
 class CInitFile
 {
-	public:
-		static const int InvalidInt;
+public:
+	static const int InvalidInt;
 
-	public:
-		CIniFile(){};
-
-		~CIniFile(){};
-
-		bool open(const char* fileName);	
-
-		std::string get(const std::string& key) const;
-
-		std::string get(const std::string& key, const std::string& defaultValue) const;
-
-		int getInt(const std::string& key, int defaultValue=(int)InvalidInt) const;
-
-		long getLong(const std::string& key, long defaultValue=(long)InvalidInt) const;
-
-		double getDouble(const std::string& key, double defaultValue=(double)InvalidInt) const;
+public:
+	CIniFile(){};
+	
+	~CIniFile(){};
+	
+	bool open(const char* fileName);	
+	
+	std::string get(const std::string& key) const;
+	
+	std::string get(const std::string& key, const std::string& defaultValue) const;
+	
+	int getInt(const std::string& key, int defaultValue=(int)InvalidInt) const;
+	
+	long getLong(const std::string& key, long defaultValue=(long)InvalidInt) const;
+	
+	double getDouble(const std::string& key, double defaultValue=(double)InvalidInt) const;
                 
 
-   private:
-    std::map<std::string, std::string>  m_key2Value;
+private:
+	std::map<std::string, std::string>  m_key2Value;
 
-		bool m_isOpen;
+	bool m_isOpen;
 
 };
 
