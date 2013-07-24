049b21eda3c62279218fb8980ab9625e4ae110d4
qelover <weiqin_wq@163.com>
2013-07-25 00:13:28 +0800

    Update fileManager.h

--- textFileMgr/fileManager.h
+++ textFileMgr/fileManager.h
@@ -1,3 +1,7 @@
+/*
+  @brief: operation about: file size, file name, etc.
+*/
+
 #ifndef FILE_MANAGER_H
 #define FILE_MANAGER_H
 
@@ -7,14 +11,14 @@ using namespace std;
 
 class CFileManager
 {
-  public:
-		static uint64_t calFileSize(const string& fileName);
-		
-		static int calFileNum(const vector<string>& fileNameVec);
-		
-		static string getFileName(const string& dir, const int iFileName);
-
-		static string getFileName(const stationId_t id, const int fileNum);
+public:
+	static uint64_t calFileSize(const string& fileName);
+	
+	static int calFileNum(const vector<string>& fileNameVec);
+	
+	static string getFileName(const string& dir, const int iFileName);
+	
+	static string getFileName(const stationId_t id, const int fileNum);
 };
 
 #endif
