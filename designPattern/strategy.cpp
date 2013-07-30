e05e3295d60d2e70ee3233f4411ff869570c81da
qelover <weiqin_wq@163.com>
2013-07-30 22:18:29 +0800

    Create strategy.cpp

---
+++ designPattern/strategy.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "Strategy.h"
+
+
+Context::Context(Strategy *pStrategy) : m_pStrategy(pStrategy)
+{
+}
+
+Context::~Context()
+{
+    delete m_pStrategy;
+    m_pStrategy = NULL;
+}
+
+void Context::ContextInterface()
+{
+    if (NULL != m_pStrategy)
+    {
+        m_pStrategy->AlgorithmInterface();
+    }
+}
+
+void ConcreateStrategyA::AlgorithmInterface()
+{
+    std::cout << "AlgorithmInterface Implemented by ConcreateStrategyA" << std::endl;
+}
